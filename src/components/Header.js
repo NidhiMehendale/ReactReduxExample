7aecdfccc043eb80b350f2f93295f0b18bcbdae2
Dipti Mehendale <diptimehendale@Diptis-MacBook-Air.local>
2023-08-23 15:36:45 +0530

    Auth Reducer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,33 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import classes from './Header.module.css';
+import { authActions } from '../store/index';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+ 
+  const logoutHandler = (e) => {
+    e.preventDefault();
+    dispatch(authActions.logout());
+  };
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {isAuth && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/"> My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+          </ul>
+          <button onClick={logoutHandler}>Logout</button>
+        </nav>
+      )}
     </header>
   );
 };
