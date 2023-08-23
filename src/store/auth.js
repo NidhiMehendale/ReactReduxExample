1d0ef3eb389c759e727795bec2873282de41a9fc
Dipti Mehendale <diptimehendale@Diptis-MacBook-Air.local>
2023-08-23 16:09:33 +0530

    final commit

---
+++ src/store/auth.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialAuthState={
+    isAuthenticated:false
+}
+const authSlice=createSlice({
+    name:'authentication',
+    initialState:initialAuthState,
+    reducers:{
+        login(state){
+            state.isAuthenticated=true; 
+          
+            console.log("thank you")
+        },
+        logout(state){
+            state.isAuthenticated=false;
+          
+            console.log('ok bye')
+        }
+    }
+});
+
+export const authActions=authSlice.actions;
+
+export default authSlice.reducer;
